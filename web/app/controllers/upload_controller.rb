453457a88eab71c8de388ec825838f385dc481c0
Bryce Boe <bbzbryce@gmail.com>
2009-01-23 11:50:35 -0800

    More progress with upload

--- web/app/controllers/upload_controller.rb
+++ web/app/controllers/upload_controller.rb
@@ -1,14 +1,13 @@
 class UploadController < ApplicationController
 
   layout "main"
+  def index
+    @uploaded = "Not_uploaded"
+  end
 
-
-      def index
-         @uploaded = "Not_uploaded"
-      end
-      def uploadTorrent
-        post = TorrentFile.save(params[:upload])
-        @uploaded = "uploaded"
-      end
-    
+  def uploadTorrent
+    post = TorrentFile.save(params[:upload])
+    @uploaded = "uploaded"
+    redirect_to(:action => "index")
+  end
 end
--- web/app/models/torrent_file.rb
+++ web/app/models/torrent_file.rb
@@ -1,11 +1,10 @@
 class TorrentFile < ActiveRecord::Base
-    def self.save(upload)
-        name =  upload['torrentfile'].original_filename
-        directory = "public/torrents"
-        # create the file path
-        path = File.join(directory, name)
-        # write the file
-        File.open(path, "wb") { |f| f.write(upload['torrentfile'].read) }
-      end
-                 
+  def self.save(upload)
+    name =  upload['torrentfile'].original_filename
+    directory = "public/torrents"
+    # create the file path
+    path = File.join(directory, name)
+    # write the file
+    File.open(path, "wb") { |f| f.write(upload['torrentfile'].read) }
+  end
 end
