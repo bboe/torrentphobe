885272b3487b3ae3ca5516dd47db1d7b8550308d
unknown <krithika@.(none)>
2009-01-23 00:42:40 -0800

    Submitting a few fixes.

--- web/app/controllers/upload_controller.rb
+++ web/app/controllers/upload_controller.rb
@@ -1,7 +1,8 @@
 class UploadController < ApplicationController
       def index
          @uploaded = "Not_uploaded"
-         render :file => 'app\views\upload\uploadTorrent.rhtml'
+          #redirect_to(:action => 'uploadTorrent')
+          render :file => 'app\views\upload\uploadTorrent.rhtml'
       end
       def uploadTorrent
         post = TorrentFile.save(params[:upload])
