6703bdddc0ec92d79ae1d2492745cfc9018f5cb6
Adam Doupe <adamdoupe@gmail.com>
2009-01-29 19:12:32 -0800

    Jeff and I added the generate_torrent_file method to the torrent model. Also added filename to get the name of the file + ".torrent". Tests to match.

--- web/app/models/torrent.rb
+++ web/app/models/torrent.rb
@@ -1,7 +1,26 @@
 class Torrent < ActiveRecord::Base
   require 'bencode'
-  #validates_presence_of :name, :size, :meta_info
+  validates_presence_of :name, :size, :data
   validates_numericality_of :size, :greater_than => 0
+
+  SECRECT_KEY = 'jeffsucksandrules78978952yuihlkf'
+
+  def filename
+    self.name + ".torrent"
+  end
+
+  def generate_torrent_file user_id
+    btorrent = BEncode.load(self.data)
+    btorrent["announce"] = self.tracker_url( user_id )
+    btorrent.bencode
+  end
+
+  def tracker_url user_id = nil
+    host = "http://localhost:3000/"
+    hash = Digest::SHA1.hexdigest( self.id.to_s + SECRECT_KEY + user_id.to_s )
+    host + "swarms/announce/" + CGI.escape(hash) + "/" + self.id.to_s + "/" + user_id.to_s
+  end
+  
   def self.updatetorrentfile(upload)
      @tempname = upload['torrentfile'].original_filename
      @tempsize = upload['torrentfile'].size
