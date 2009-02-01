48fa0830f61263fa598822950c3c5edd197b99f3
Jonathan Kupferman <jkupferman@compie586.(none)>
2009-02-01 00:40:50 -0800

    Upadated tabs to reflect current controllers in the system and link to them propertly.

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include TagsHelper
+
+  def tab_class tab_controller
+    (controller.controller_name.to_sym == tab_controller) ? "main" : "bg"
+  end
 end
