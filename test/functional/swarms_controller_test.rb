954eed7e5d67ac96bc17b5cd5d2559791f0c230a
Bryce Boe <bryce@ubuntu.(none)>
2009-02-19 19:54:13 -0800

    Fixed ip thing in swarms

--- test/functional/swarms_controller_test.rb
+++ test/functional/swarms_controller_test.rb
@@ -7,16 +7,21 @@ class SwarmsControllerTest < ActionController::TestCase
   end
 
   test "announce right params" do
-    get :a, {:id => ['yXZlLdfEp1K9KtZKefIONQ'],
-                    :peer_id => "TESTID", :port => "6882"}
-    assert_response :success
+    good = swarms(:good)
+    get(:a, {:id => ["YrzCSV+QpwfcSs", "AYxRK3g"],
+          :peer_id => good.peer_id, :port => good.port, :ip => good.ip_address,
+          :info_hash => "JEFF_IS_THE_SHIZNIT1"})
+    assert_response 500
   end
+
+
 
   test "announce results test" do
     good = swarms(:good)
-    get :a, {:id => ['yXZlLdfEp1K9KtZKefIONQ'],
-                    :peer_id => good.peer_id, :port => good.port, :ip => good.ip_address}
-
+    get(:a, {:id => ["YrzCSV+QpwfcSs", "AYxRK3g"],
+          :peer_id => good.peer_id, :port => good.port, :ip => good.ip_address,
+          :info_hash => "JEFF_IS_THE_SHIZNIT6"})
+    
     result = BEncode.load(@response.body)
     assert_equal good.peer_id, result["peers"][0]["peer id"]
     assert_equal good.ip_address, result["peers"][0]["ip"]
@@ -26,15 +31,18 @@ class SwarmsControllerTest < ActionController::TestCase
   end
 
   test "announce bad encrypted64" do
-    get :a, {:id => ["jeffrockers!"], :peer_id => "TESTID", :port => "6882"}
+    get(:a, {:id => ["jeffrockers!"], :peer_id => "TESTID", :port => "6882",
+          :info_hash => "JEFF_IS_THE_SHIZNIT3", :ip => "127.0.0.1"})
     result = BEncode.load(@response.body)
     assert_equal "Invalid announce URL.", result["failure"]
   end
 
   test "announce new completed to swarm" do
     assert_difference("Swarm.count", 1) do
-      get :a, {:id => ['VaFS3pGFGfyOBi1Xp','wcgA'],
-        :peer_id => "NEW_PEER", :port => 8080, :event => "completed"}
+      get(:a, {:id => ["YrzCSV+QpwfcSs", "AYxRK3g"],
+            :peer_id => "NEW_PEER", :port => 8080, :event => "completed", 
+            :info_hash => "JEFF_IS_THE_SHIZNIT6", :ip => "127.0.0.1"})
+      result = BEncode.load(@response.body)
     end
     assert_response :success
     result = BEncode.load(@response.body)
@@ -44,8 +52,10 @@ class SwarmsControllerTest < ActionController::TestCase
 
   test "announce new seeder started to swarm" do
     assert_difference("Swarm.count", 1) do
-      get :a, {:id => ['VaFS3pGFGfyOBi1Xp','wcgA'],
-        :peer_id => "NEW_PEER", :port => 8080, :event => "started", :left => "0"}
+      get(:a, {:id => ["YrzCSV+QpwfcSs", "AYxRK3g"],
+            :peer_id => "NEW_PEER", :port => 8080, :event => "started",
+            :left => "0", :info_hash => "JEFF_IS_THE_SHIZNIT6",
+            :ip => "127.0.0.1"})
     end
     assert_response :success
     result = BEncode.load(@response.body)
@@ -55,8 +65,10 @@ class SwarmsControllerTest < ActionController::TestCase
 
   test "announce new started to swarm" do
     assert_difference("Swarm.count", 1) do
-      get :a, {:id => ['VaFS3pGFGfyOBi1Xp','wcgA'],
-        :peer_id => "NEW_PEER", :port => 8080, :event => "started"}
+
+      get(:a, {:id => ["YrzCSV+QpwfcSs", "AYxRK3g"],
+            :peer_id => "NEW_PEER", :port => 8080, :event => "started",
+            :info_hash => "JEFF_IS_THE_SHIZNIT6", :ip => "127.0.0.1"})
     end
     assert_response :success
     result = BEncode.load(@response.body)
