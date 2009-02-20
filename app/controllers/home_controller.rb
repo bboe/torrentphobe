3b0750c4ade1d31f75e691b5257edaf608627e8c
Jonathan Kupferman <jkupferman@compie586.(none)>
2009-02-20 02:10:35 -0800

    Added home page for users, made tables look nicer and have hover effects. Fixed categories sorting which was broken.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  layout "main"
+  def index
+    @current_user = get_current_user
+
+    ordering = handle_sort params
+    @torrents = @current_user.my_torrents
+
+    @torrents.sort! { |a,b| a.created_at <=> b.created_at }
+    @torrents = @torrents.reverse[0..5]
+
+    @new_users = @current_user.friends.map { |user| user if user.created_at < 7.days.ago }.compact
+  
+    respond_to do |format|
+        format.html # index.html.erb
+    end
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,15 +5,15 @@ class Category < ActiveRecord::Base
   def self.sort_torrents(torrents, category, direction)
     if torrents
        case category
-       when "category_id"
-	  torrents = torrents.sort_by { |torrent| torrent.category_id } 
+       when "category"
+         torrents = torrents.sort_by { |torrent| torrent.category.name } 
        when "size"
-	  torrents = torrents.sort_by { |torrent| torrent.size } 
+         torrents = torrents.sort_by { |torrent| torrent.size } 
        else
-	  torrents = torrents.sort_by { |torrent| torrent.name.downcase } 
+         torrents = torrents.sort_by { |torrent| torrent.name.downcase } 
        end
        if direction == "up"
-	 torrents = torrents.reverse
+         torrents = torrents.reverse
        end
     else
       torrents = []
